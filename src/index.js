3b2534ad9277784b027be346921a8fc63c1f252d
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-02-22 19:57:35 +0530

    Add basic functions to fetch Weather JSON data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log("Test successful");
+const getCoords = async () => {
+  try {
+    const response = await fetch(
+      "https://api.openweathermap.org/geo/1.0/direct?q=Trichy&appid=aaef257495c3f595680e4bb46f989362",
+      { mode: "cors" },
+    );
+
+    const data = await response.json();
+    console.log("Data", data[0]);
+    console.log("lat :", data[0].lat, " ", "lon :", data[0].lon);
+    const coords = { lat: data[0].lat, lon: data[0].lon };
+    return coords;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+const getWeather = async (coordsData) => {
+  const coords = await coordsData;
+  console.log("Coords Data", coords);
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=aaef257495c3f595680e4bb46f989362`,
+      { mode: "cors" },
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getWeather(getCoords());
