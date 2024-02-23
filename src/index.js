b96019ca56167a9874a010dac8addabe8ca2db58
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-02-23 21:34:30 +0530

    Add functionality to get required current data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+const parseWeatherData = (data) => {
+  const { current } = data;
+  const weatherData = {
+    currentData: {
+      currentTime: current.dt,
+      currentSunRise: current.sunrise,
+      currentTemp: current.temp,
+      currentFeelsLike: current.feels_like,
+      currentHumidity: current.humidity,
+      currentUVIndex: current.uvi,
+      currentWindSpeed: current.wind_speed,
+      currentWindDeg: current.wind_deg,
+      currentWeather: current.weather[0].main,
+    },
+  };
+  return weatherData;
+};
+
 const getCoords = async () => {
   try {
     const response = await fetch(
@@ -16,19 +34,20 @@ const getCoords = async () => {
   }
 };
 
-const getWeather = async (coordsData) => {
+const getWeatherData = async (coordsData) => {
   const coords = await coordsData;
   console.log("Coords Data", coords);
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=aaef257495c3f595680e4bb46f989362`,
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${coords.lat}&lon=${coords.lon}&units=metric&appid=aaef257495c3f595680e4bb46f989362`,
       { mode: "cors" },
     );
     const data = await response.json();
-    console.log(data);
+    const weatherData = parseWeatherData(data);
+    console.log(weatherData.currentData);
   } catch (err) {
     console.log(err);
   }
 };
 
-getWeather(getCoords());
+getWeatherData(getCoords());
