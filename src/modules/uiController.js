e74a75daaa29ffd5129985a3ea76262ec3e04de2
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-02-25 11:41:44 +0530

    Make the current weather data displayed

--- src/modules/uiController.js
+++ src/modules/uiController.js
@@ -1,6 +1,35 @@
 import weatherAPI from "./weatherAPI";
 
 const uiController = (() => {
+  const displayCurrent = (currentData) => {
+    const currentTime = document.querySelector(".currentTime");
+    currentTime.textContent = `Current Time : ${currentData.currentTime}`;
+
+    const currentSunRise = document.querySelector(".currentSunRise");
+    currentSunRise.textContent = `Sun Rise : ${currentData.currentSunRise}`;
+
+    const currentTemp = document.querySelector(".currentTemp");
+    currentTemp.textContent = `Temp : ${currentData.currentTemp}`;
+
+    const currentFeelsLike = document.querySelector(".currentFeelsLike");
+    currentFeelsLike.textContent = `Feels like : ${currentData.currentFeelsLike}`;
+
+    const currentHumidity = document.querySelector(".currentHumidity");
+    currentHumidity.textContent = `Humidity : ${currentData.currentHumidity}`;
+
+    const currentUVIndex = document.querySelector(".currentUVIndex");
+    currentUVIndex.textContent = ` UVIndex : ${currentData.currentUVIndex}`;
+
+    const currentWindSpeed = document.querySelector(".currentWindSpeed");
+    currentWindSpeed.textContent = `Wind Speed : ${currentData.currentWindSpeed}`;
+
+    const currentWindDeg = document.querySelector(".currentWindDeg");
+    currentWindDeg.textContent = `Wind Drag : ${currentData.currentWindDeg}`;
+
+    const currentWeather = document.querySelector(".currentWeather");
+    currentWeather.textContent = `Weather : ${currentData.currentWeather}`;
+  };
+
   const displayData = () => {
     const searchBtn = document.querySelector("#searchBtn");
     searchBtn.addEventListener("click", async () => {
@@ -8,7 +37,7 @@ const uiController = (() => {
       const obtainedData = await weatherAPI.getWeatherData(searchInput);
 
       if (obtainedData) {
-        console.log("Current data ", obtainedData.currentData);
+        displayCurrent(obtainedData.currentData);
         console.log("Hourly data", obtainedData.hourlyData);
         console.log("Daily data", obtainedData.dailyData);
       } else {
